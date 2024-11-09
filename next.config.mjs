60564c283988c7d5c2c688ba3e09dd25454415ef
Dave Gill <david.g.h.gill@gmail.com>
2024-11-09 21:09:00 +0000

    Setup to run locally

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Other config options...
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',  // Match any API route
+        destination: 'http://localhost:3001/api/:path*',  // Forward to Rust server
+         has: [
+           {
+             type: 'header',
+             key: 'host',
+             value: 'localhost:3000',
+           },
+         ],
+      }
+    ];
+  },
+};
 
 export default nextConfig;
